77070fd86ab2e680254c30eef065ddfe0ebfb2bd
jope25 <jolmari10@gmail.com>
2017-12-10 13:33:22 +0200

    setting up

---
+++ src/main/java/jope/webnews/WebNewsApp.java
@@ -0,0 +1,13 @@
+
+package jope.webnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNewsApp {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebNewsApp.class, args);
+    }
+}
